f1473abef95a6b586b767f4c0d340a29cf03b708
RoberthLagos <roberthlagos23@gmail.com>
2024-11-08 17:01:49 -0600

    Creando Clase main para ejecutar codigo - java

---
+++ ProyectoGrupo5/src/main/java/Pmain.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+public class PMain {
+    public static void pmain(String[] args) {
+        SubDoctor Doctor = new SubDoctor("Pedro", 45, "Cardiologia");
+        Doctor.Saludar();
+        Doctor.MostrarInfo();
+    }
+}
+
